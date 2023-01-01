729779024e631c824ef39542d0fb6748bbb6e385
Kazuki Kyakuno <kyakuno@axell.co.jp>
2023-01-01 20:12:15 +0900

    Remove unused code

--- ailia-apps-people-counter.py
+++ ailia-apps-people-counter.py
@@ -35,31 +35,6 @@ args = update_parser(parser)
 
 
 # ======================
-# Secondaty Functions
-# ======================
-
-def get_colors(n, colormap="gist_ncar"):
-    # Get n color samples from the colormap, derived from: https://stackoverflow.com/a/25730396/583620
-    # gist_ncar is the default colormap as it appears to have the highest number of color transitions.
-    # tab20 also seems like it would be a good option but it can only show a max of 20 distinct colors.
-    # For more options see:
-    # https://matplotlib.org/examples/color/colormaps_reference.html
-    # and https://matplotlib.org/users/colormaps.html
-
-    colors = cm.get_cmap(colormap)(np.linspace(0, 1, n))
-    # Randomly shuffle the colors
-    np.random.shuffle(colors)
-    # Opencv expects bgr while cm returns rgb, so we swap to match the colormap (though it also works fine without)
-    # Also multiply by 255 since cm returns values in the range [0, 1]
-    colors = colors[:, (2, 1, 0)] * 255
-
-    return colors
-
-
-num_colors = 50
-vis_colors = get_colors(num_colors)
-
-# ======================
 # Video
 # ======================
 
